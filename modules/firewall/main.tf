44669f494df857ca0a0d21851b515d70f45fd470
Muhamadyusuf2020 <167102823+Muhamadyusuf2020@users.noreply.github.com>
2025-01-13 14:20:48 +0200

    Update main.tf

--- modules/firewall/main.tf
+++ modules/firewall/main.tf
@@ -24,7 +24,7 @@ resource "google_compute_firewall" "allow-http" {
 
   allow {
     protocol = "tcp"
-    ports    = ["80","8080"]
+    ports    = ["80","8080",5000]
   }
 
   target_tags   = ["http-server"]
